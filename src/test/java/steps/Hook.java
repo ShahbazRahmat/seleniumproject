3d528debd19f2d115556f16d2fbd888576f34792
ShahbazRahmat <shahbazrahmat@gmail.com>
2017-03-28 15:50:51 +0500

    Update Hook.java file
    Update Hook.java file

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -22,10 +22,10 @@ public class Hook extends BaseUtil{
     @Before
     public void InitializeTest() {
 
-         /*System.out.println("Opening the browser : Firefox");
+         System.out.println("Opening the browser : Firefox");
 
         System.setProperty("webdriver.firefox.marionette", "C:\\Libs\\geckodriver.exe");
-        base.Driver = new FirefoxDriver();*/
+        base.Driver = new FirefoxDriver();
 		
 
 
@@ -33,11 +33,6 @@ public class Hook extends BaseUtil{
        /* System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
         base.Driver = new ChromeDriver();*/
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
-		ChromeOptions opt = new ChromeOptions();
-		opt.addArguments("disable-extensions");
-		opt.addArguments("--start-maximized");
-		WebDriver driver = new ChromeDriver(opt);
         
     }
 
